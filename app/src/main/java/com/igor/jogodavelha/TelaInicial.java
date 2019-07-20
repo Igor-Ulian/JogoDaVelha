a7173da80e4c1e72f4f714d5fa9d301891cab7df
Igor Ulian <IgorUlian@outlook.com>
2019-07-20 00:29:37 -0300

    - Logo image translated into English, Spanish and Portuguese.
    - Game Translated to Spanish
    
    - Background changed

--- app/src/main/java/com/igor/jogodavelha/TelaInicial.java
+++ app/src/main/java/com/igor/jogodavelha/TelaInicial.java
@@ -5,16 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Locale;
 
 public class TelaInicial extends AppCompatActivity {
 
     Intent tela;
+    ImageView img;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+        Locale loc = Locale.getDefault();
+        String lg = loc.getLanguage();
+        if(lg.contains("pt")){  //Verify language from set image logo
+            img = findViewById(R.id.imgLogo);
+            img.setImageResource(R.drawable.strlogopt);
+        } else if(lg.contains("es")){ //Verify language from set image logo
+            img = findViewById(R.id.imgLogo);
+            img.setImageResource(R.drawable.strlogoes);
+        }
     }
 
     public void click2(View view) { //2 Players Click
--- app/src/main/java/com/igor/jogodavelha/telaNomeJogadores.java
+++ app/src/main/java/com/igor/jogodavelha/telaNomeJogadores.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Locale;
 
 public class telaNomeJogadores extends AppCompatActivity {
 
     EditText jg1, jg2;
     Intent tela;
+
+    ImageView img;
 
     String nj1 = "";
     String nj2 = "";
@@ -21,6 +26,17 @@ public class telaNomeJogadores extends AppCompatActivity {
         setContentView(R.layout.activity_tela_nome_jogadores2);
         jg1 = findViewById(R.id.inputJogador1);
         jg2 = findViewById(R.id.inputJogador2);
+
+        Locale loc = Locale.getDefault();
+        String lg = loc.getLanguage();
+
+        if(lg.contains("pt")){  //Verify language from set image logo
+            img = findViewById(R.id.imgLogo);
+            img.setImageResource(R.drawable.strlogopt);
+        } else if(lg.contains("es")){ //Verify language from set image logo
+            img = findViewById(R.id.imgLogo);
+            img.setImageResource(R.drawable.strlogoes);
+        }
     }
 
     public void iniciarClick(View view) { //Start Click
@@ -34,7 +50,7 @@ public class telaNomeJogadores extends AppCompatActivity {
             ok = false;
             jg2.setError(getString(R.string.msgErro));
         }
-        if(ok != true) return;
+        if(!ok) return;
 
         nj1 = jg1.getText().toString();
         nj2 = jg2.getText().toString();
